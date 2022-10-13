6b038ebd80e1177eafa1f9a08c1fc7490db001f4
Elizabeth Madrigal <elimad99@gmail.com>
2022-10-12 18:59:20 -0700

    server working

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
 const express = require('express');
-const db = require('./db/connection');
+const session = require("express-session");
+const exphbs = require("express-handelbars");
+require("dotenv").config(); 
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+const routes = require("./routes");
+app.use(routes);
+
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+});
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
